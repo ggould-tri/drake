54ad877d7748cd690760bfd0a2ecd66a9a3eff9f
Grant Gould <ggould@tri.global>
2019-07-30 17:09:22 -0400

    WIP NOCOMMIT kinda working

--- multibody/plant/element_collection.h
+++ multibody/plant/element_collection.h
@@ -29,90 +29,185 @@ template <template <typename> class ElementType,
     typename T, typename ElementIndexType>
 class MultibodyElementCollection<ElementType<T>, ElementIndexType> {
  public:
-  /// Construct a zero-element collection.
-  MultibodyElementCollection() {}
-
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(MultibodyElementCollection)
-
   using ThisType = MultibodyElementCollection<ElementType<T>, ElementIndexType>;
   using ElementFilterFunc = std::function<bool(const ElementType<T>&)>;
   using IndexFilterFunc = std::function<bool(ElementIndexType)>;
-  using ConstIteratorType =
-      typename std::vector<const ElementType<T>*>::const_iterator;
+  using ElementLookupFunc =
+      std::function<const ElementType<T>&(ElementIndexType)>;
+  using ElementVecType = std::vector<const ElementType<T>*>;
+  using ConstIteratorType = typename ElementVecType::const_iterator;
+
+  /// Construct a collection of no elements
+  ///
+  /// @p plant will be aliased and must remain valid for the lifetime of this
+  /// collection.
+  explicit MultibodyElementCollection(const MultibodyPlant<T>* plant)
+      : plant_(plant) {}
+
+  /// Given an MBP, a lookup function (eg, `MultibodyPlant::get_body`), and a
+  /// maximum index (eg, `MultibodyPlant::num_bodies`), return an
+  /// `ElementCollection` with all of the elements.
+  static ThisType CollectAll(const MultibodyPlant<T>* plant,
+                             ElementLookupFunc id_lookup_func,
+                             int num_indexes) {
+    ElementVecType elements;
+    for (int i = 0; i < num_indexes; i++) {
+      elements.push_back(&id_lookup_func(ElementIndexType(i)));
+    }
+    return ThisType(plant, elements);
+  }
+
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(MultibodyElementCollection)
 
   inline ConstIteratorType begin() const { return elements_.begin(); }
   inline ConstIteratorType end() const { return elements_.end(); }
   int size() const { return static_cast<int>(elements_.size()); }
 
+  /// Assert that there is only one member of this collection and return it.
+  const ElementType<T>& OnlyOne() const {
+    DRAKE_DEMAND(size() == 1);
+    return **begin();
+  }
+
+  /// @return a collection of all members `m` of this collection for which
+  /// `func(m)` is true.
+  // TODO(ggould) Just use `remove_copy_if` once we have C++20.
   ThisType Filter(ElementFilterFunc func) const {
-    auto new_elements = elements_;
-    std::remove_if(new_elements.begin(), new_elements.end(),
-                   [&](const auto& element){ return !func(*element); });
-    return ThisType(new_elements);
+    ElementVecType new_elements = elements_;
+    new_elements.erase(
+        std::remove_if(new_elements.begin(), new_elements.end(),
+                       [&](const auto& element){ return !func(*element); }),
+        new_elements.end());
+    return ThisType(plant_, new_elements);
+  }
+
+  /// @return a collection of all members `m` of this collection for which
+  /// `m.name()` is @p name.
+  ThisType Named(const std::string& name) const {
+    return Filter([name](const ElementType<T>& e) { return e.name() == name; });
   }
 
  private:
-  explicit MultibodyElementCollection(
+  MultibodyElementCollection(
+      const MultibodyPlant<T>* plant,
       const std::vector<const ElementType<T>*>& elements)
-      : elements_(elements) {}
+      : plant_(plant),
+        elements_(elements) {}
 
-  std::vector<const ElementType<T>*> elements_;
+  const MultibodyPlant<T>* plant_;
+  const ElementVecType elements_;
 };
 
-template <typename T>
-using BodyCollection = MultibodyElementCollection<Body<T>, BodyIndex>;
-template class MultibodyElementCollection<Body<double>, BodyIndex>;
 
 template <typename T>
 using JointCollection = MultibodyElementCollection<Joint<T>, JointIndex>;
-template class MultibodyElementCollection<Joint<double>, JointIndex>;
+
+template <typename T>
+using BodyCollection = MultibodyElementCollection<Body<T>, BodyIndex>;
+
+template <typename T>
+BodyCollection<T> Bodies(const MultibodyPlant<T>* plant) {
+  return BodyCollection<T>::CollectAll(
+      plant,
+      [&](BodyIndex id) -> const Body<T>& { return plant->get_body(id); },
+      plant->num_bodies());
+}
 
 template <typename T>
 using JointActuatorCollection =
     MultibodyElementCollection<JointActuator<T>, JointIndex>;
-template class MultibodyElementCollection<JointActuator<double>, JointIndex>;
 
 template <typename T>
 using FrameCollection = MultibodyElementCollection<Frame<T>, FrameIndex>;
-template class MultibodyElementCollection<Frame<double>, FrameIndex>;
 
 
 /// A generic class for vector-like collections of MultibodyTree TypeSafeIndex
 /// types with no public element type (eg, `ModelIndexInstance`).
-template <typename ElementIndexType>
+template <typename ElementIndexType, typename T>
 class MultibodyIndexCollection {
  public:
-  /// Construct a zero-element collection.
-  MultibodyIndexCollection() {}
-
-  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(MultibodyIndexCollection)
-
-  using ThisType = MultibodyIndexCollection<ElementIndexType>;
+  using ThisType = MultibodyIndexCollection<ElementIndexType, T>;
   using IndexFilterFunc = std::function<bool(ElementIndexType)>;
   using ConstIteratorType =
       typename std::vector<ElementIndexType>::const_iterator;
+
+  /// Construct a collection of no elements
+  ///
+  /// @p plant will be aliased and must remain valid for the lifetime of this
+  /// collection.
+  explicit MultibodyIndexCollection(const MultibodyPlant<T>* plant)
+      : plant_(plant) {}
+
+  MultibodyIndexCollection(
+      const MultibodyPlant<T>* plant,
+      const std::vector<ElementIndexType>& indexes)
+      : plant_(plant),
+        indexes_(indexes) {}
+
+  /// Given an MBP and a maximum index (eg, `MultibodyPlant::num_bodies`),
+  /// return an `ElementCollection` with all of the elements.
+  static ThisType CollectAll(const MultibodyPlant<T>* plant,
+                             int num_indexes) {
+    std::vector<ElementIndexType> indexes;
+    for (int i = 0; i < num_indexes; i++) {
+      indexes.push_back(ElementIndexType(i));
+    }
+    return ThisType(plant, indexes);
+  }
+
+  DRAKE_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(MultibodyIndexCollection)
 
   inline ConstIteratorType begin() const { return indexes_.begin(); }
   inline ConstIteratorType end() const { return indexes_.end(); }
   int size() const { return static_cast<int>(indexes_.size()); }
 
+  /// Assert that there is only one member of this collection and return it.
+  ElementIndexType OnlyOne() const {
+    DRAKE_DEMAND(size() == 1);
+    return *begin();
+  }
+
+  /// @return a collection of all members `id` of this collection for which
+  /// `func(id)` is true.
   ThisType Filter(IndexFilterFunc func) const {
-    auto new_indexes = indexes_;
-    std::remove_if(new_indexes.begin(), new_indexes.end(),
-                   [&](const auto& index){ return !func(*index); });
-    return ThisType(new_indexes);
+    std::vector<ElementIndexType> new_indexes = indexes_;
+    new_indexes.erase(
+        std::remove_if(new_indexes.begin(), new_indexes.end(),
+                       [&](const auto& index){ return !func(index); }),
+        new_indexes.end());
+    return ThisType(plant_, new_indexes);
+  }
+
+  /// Implicit partial specialization: ModelInstanceIndex'es have names.
+  ThisType Named(const std::string& name) const {
+    return Filter([&](ElementIndexType id) {
+        return plant_->GetModelInstanceName(id) == name; });
   }
 
  private:
-  explicit MultibodyIndexCollection(
-      const std::vector<ElementIndexType>& indexes)
-      : indexes_(indexes) {}
-
+  const MultibodyPlant<T>* plant_;
   std::vector<ElementIndexType> indexes_;
 };
 
-using ModelInstanceCollection = MultibodyIndexCollection<ModelInstanceIndex>;
-using ForceElementCollection = MultibodyIndexCollection<ForceElementIndex>;
+template <typename T>
+using ModelInstanceCollection = MultibodyIndexCollection<ModelInstanceIndex, T>;
+
+template <typename T>
+ModelInstanceCollection<T> ModelInstances(const MultibodyPlant<T>* plant) {
+  return ModelInstanceCollection<T>::CollectAll(
+      plant,
+      plant->num_model_instances());
+}
+
+template <typename T>
+using ForceElementCollection = MultibodyIndexCollection<ForceElementIndex, T>;
+
+template <typename T>
+ForceElementCollection<T> ForceElements(const MultibodyPlant<T>* plant) {
+  return ForceElementCollection<T>::CollectAll(
+      plant,
+      plant->num_force_elements());
+}
 
 }  // namespace multibody
 }  // namespace drake
--- multibody/plant/test/element_collection_test.cc
+++ multibody/plant/test/element_collection_test.cc
@@ -12,25 +12,80 @@ namespace {
 
 // Test construction and destruction without elements.
 // For simplicity this is the only testing we do with all of the scalartypes.
-template <typename CollectionType>
+template <template<typename ScalarType> typename CollectionType, typename T>
 void SmokeTestCollection() {
-  CollectionType collection;
+  MultibodyPlant<T> plant;
+  CollectionType<T> collection(&plant);
   EXPECT_EQ(collection.size(), 0);
   for (const auto x: collection) { EXPECT_TRUE(x != x); }
 }
 template<template<typename ScalarType> typename CollectionType>
 void SmokeTestCollectionAllScalarTypes() {
-  SmokeTestCollection<CollectionType<double>>();
-  SmokeTestCollection<CollectionType<AutoDiffXd>>();
-  SmokeTestCollection<CollectionType<symbolic::Expression>>();
+  SmokeTestCollection<CollectionType, double>();
+  SmokeTestCollection<CollectionType, AutoDiffXd>();
+  SmokeTestCollection<CollectionType, symbolic::Expression>();
 }
 GTEST_TEST(ElementCollection, SmokeTest) {
   SmokeTestCollectionAllScalarTypes<BodyCollection>();
   SmokeTestCollectionAllScalarTypes<JointCollection>();
   SmokeTestCollectionAllScalarTypes<JointActuatorCollection>();
   SmokeTestCollectionAllScalarTypes<FrameCollection>();
-  SmokeTestCollection<ModelInstanceCollection>();
-  SmokeTestCollection<ForceElementCollection>();
+  SmokeTestCollectionAllScalarTypes<ModelInstanceCollection>();
+  SmokeTestCollectionAllScalarTypes<ForceElementCollection>();
+}
+
+// Load up a fixture and take a look around an element collection.
+TEST_F(MultibodyPlantFixture, SimpleBodyCollectionTest) {
+  plant.Finalize();
+
+  // Check that the begin/end iterators work right.
+  constexpr int kNumBodies = 143;  // Number of bodies in the fixture.
+  auto bodies = Bodies(&plant);
+  EXPECT_EQ(bodies.size(), kNumBodies);
+  EXPECT_EQ(bodies.size(), bodies.end() - bodies.begin());
+
+  // Can we do a `for` loop?
+  std::set<BodyIndex> seen;
+  for (const Body<double>* body: bodies) {
+    seen.insert(body->index());
+  }
+  EXPECT_EQ(seen.size(), kNumBodies);
+
+  // Test out the "Named" selector (this also transitively tests "Filter").
+  EXPECT_EQ(bodies.Named("pelvis").size(), 2);
+  EXPECT_EQ(bodies.Named("main_body").OnlyOne().index(), mug->index());
+}
+
+
+// Do likewise for an index collection.
+TEST_F(MultibodyPlantFixture, SimpleModelInstanceCollectionTest) {
+  plant.Finalize();
+
+  // Check that the begin/end iterators work right.
+  constexpr int kNumModelInstances = 6;  // 2xatlas, mug, table, default, world
+  auto model_instances = ModelInstances(&plant);
+  EXPECT_EQ(model_instances.size(), kNumModelInstances);
+  EXPECT_EQ(model_instances.size(),
+            model_instances.end() - model_instances.begin());
+
+  // Can we do a `for` loop?
+  std::set<ModelInstanceIndex> seen;
+  for (const ModelInstanceIndex model_instance: model_instances) {
+    seen.insert(model_instance);
+  }
+  EXPECT_EQ(seen.size(), kNumModelInstances);
+
+  // Test out the "Named" selector (this also transitively tests "Filter").
+  EXPECT_EQ(model_instances.Named("Atlas1").size(), 1);
+  EXPECT_EQ(model_instances.Named("Atlas2").OnlyOne(), atlas_model2);
+
+  // Test that we can construct a ForceElementCollection even though it is not
+  // fully specialized (has no `Named`).
+  ForceElementCollection<double> force_elements = ForceElements(&plant);
+  EXPECT_EQ(force_elements.size(), 1);  // Gravity exists.
+  // If we said `force_elements.Named("foo");` here, we'd get a compile error,
+  // which is correct.  I can't unit test that something doesn't compile, so
+  // you have to take my word for it.
 }
 
 }  // namespace
